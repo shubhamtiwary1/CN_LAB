afbfa89771c08dfd102da909a6fa4447cbd7c162
shubhamtiwary1 <114869477+shubhamtiwary1@users.noreply.github.com>
2023-11-21 13:53:12 +0530

    Add files via upload

---
+++ Java/EXP5/src/exp5/obj2.java
@@ -0,0 +1,25 @@
+package exp5;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class obj2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			URL www = new URL ("http://www.ibiblio.org/");
+			URL ibiblio = new URL("http://ibiblio.org/");
+			if(ibiblio.equals(www)) {
+				System.out.println(ibiblio + " is same as " + www);
+			}
+			else {
+				System.out.println(ibiblio + "is not the same as " + www);
+			}
+		}
+		catch(MalformedURLException ex) {
+			System.err.println(ex);
+		}
+	}
+
+}
